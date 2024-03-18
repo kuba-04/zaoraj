9ea48811b82cb8f07ac237c11846ea857848def7
kuba <jakub.gladysz1@gmail.com>
2024-03-18 20:00:43 +0100

    add pipeline to push image

--- src/main/java/pl/zaoraj/adapter/inbound/RiposteController.java
+++ src/main/java/pl/zaoraj/adapter/inbound/RiposteController.java
@@ -1,21 +1,19 @@
 package pl.zaoraj.adapter.inbound;
 
 import org.springframework.ai.chat.ChatClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class JokingController {
+public class RiposteController {
 
 	private static final String PROMPT_TEMPLATE = "Mam pewnien przesadzony problem i potrzebuję żeby ktoś mnie doprowadził do " +
 			"pionu ciętą ripostą. Najlepiej sarkastycznie i śmiesznie. Możesz skorzystać z najlepszych ripost jakie znasz " +
 			"z filmów i historycznych wypowiedzi. Mój problem to: ";
 	private final ChatClient chatClient;
 
-	@Autowired
-	public JokingController(ChatClient chatClient) {
+	public RiposteController(ChatClient chatClient) {
 		this.chatClient = chatClient;
 	}
 
